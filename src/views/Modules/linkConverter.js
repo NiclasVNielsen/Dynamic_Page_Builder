3f0838b496d0ee0c1259b4701e63b41898b671d0
NiclasVNielsen <Niclasvnielsen@gmail.com>
2023-09-18 17:40:49 +0200

    more links in same paragraph!

--- src/views/Modules/linkConverter.js
+++ src/views/Modules/linkConverter.js
@@ -8,28 +8,25 @@ export default async function convertLinks () {
             fields.forEach(field => {
                 let fieldData = field.innerHTML
 
-                //.replace(/[|&;$%@"<>()+,]/g, "")
-
                 const regexResult = regex.exec(fieldData)
 
                 if(regexResult != null){
-                    let data = regexResult[0] /* Loop here */
-                    data = data.substring(1, data.length-1);
-
-                    data = data.split('@')
-                    if(data.length != 2){
-                        console.error(data)
-                        console.error("Ehmm this link is on fire!")
-                    }else{
-                        data[0] = data[0].trim()
-                        data[1] = data[1].trim()
-                        
-                        field.innerHTML = fieldData.replace(`${regexResult[0]}`, `<a href="${data[1]}">${data[0]}</a>`)
-                    }
+                    regexResult.forEach(result => {
+                        let data = result
+                        data = data.substring(1, data.length-1);
+    
+                        data = data.split('@')
+                        if(data.length != 2){
+                            console.error(data)
+                            console.error("Ehmm this link is on fire!")
+                        }else{
+                            data[0] = data[0].trim()
+                            data[1] = data[1].trim()
+                            
+                            field.innerHTML = fieldData.replace(`${regexResult[0]}`, `<a href="${data[1]}">${data[0]}</a>`)
+                        }
+                    })
                 }
-
-                /* regex.exec(field) */
-
             })
 
             resolve("Success!")
